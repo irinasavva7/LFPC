666674a77e52c1ece675b738ea81359ec31f6d1e
Irina Savva <46068008+irinasavva7@users.noreply.github.com>
2020-05-28 14:05:15 +0300

    Add files via upload

---
+++ Laboratory 2/src/automaton/DFA.java
@@ -0,0 +1,7 @@
+package automaton;
+
+public class DFA extends automaton.Automaton {
+    DFA() {
+
+    }
+}
---
+++ Laboratory 2/src/automaton/Transition.java
@@ -0,0 +1,8 @@
+package automaton;
+
+public class Transition {
+    java.lang.String letter;
+    automaton.State next;
+
+    Transition(java.lang.String letter, automaton.State next) { /* compiled code */ }
+}
