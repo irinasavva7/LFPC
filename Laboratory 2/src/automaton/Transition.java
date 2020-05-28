27b270ed7aa8e069967efcae662448d69d5d4e18
Irina Savva <46068008+irinasavva7@users.noreply.github.com>
2020-05-28 14:19:03 +0300

    Delete Transition.java

--- Laboratory 2/src/automaton/Transition.java
+++
@@ -1,8 +0,0 @@
-package automaton;
-
-public class Transition {
-    java.lang.String letter;
-    automaton.State next;
-
-    Transition(java.lang.String letter, automaton.State next) { /* compiled code */ }
-}
